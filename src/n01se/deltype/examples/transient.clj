ea0f88561fd0943fe658bb5cb98cfda3612d66a9
Jonathan Claggett <jclaggett@lonocloud.com>
2014-04-11 08:43:00 -0400

    Add new example showing how to support transients

---
+++ src/n01se/deltype/examples/transient.clj
@@ -0,0 +1,41 @@
+(ns n01se.deltype.examples.transient
+  (:require [clojure.test :refer [deftest]]
+            [clojure.pprint :refer [pprint]]
+            [n01se.deltype :refer [deltype]]))
+
+(use 'clojure.repl)
+
+"Problem: defining a new type that delegates the transient method to a
+ subfield will cause the new type to be 'lost' when transient returns
+ a new transient value that doesn't know about the top level new
+ type."
+
+(deltype NewMap [map-field]
+         :delegate [map-field clojure.lang.PersistentHashMap])
+
+(-> (NewMap. {:q "initial-value"})
+    empty
+    (assoc :a 1)
+    (doto (-> class prn)) ;=> NewMap
+    )
+
+"Solution 1: Unify peristent and transient methods into the new type.
+ This means that the new type is maintained through all stages of the
+ process."
+
+(deltype NewMap [map-field]
+  :delegate [map-field clojure.lang.PersistentHashMap
+             map-field clojure.lang.ITransientMap]
+
+  ;; explicitly make these two methods monoidal.
+  clojure.lang.IEditableCollection
+  (asTransient [_] (NewMap. (.asTransient map-field)))
+
+  clojure.lang.ITransientCollection
+  (persistent [_] (NewMap. (.persistent map-field))))
+
+(-> (NewMap. {:q "initial-value"})
+    empty
+    (assoc :a 1)
+    (doto (-> class prn)) ;=> NewMap
+    )
